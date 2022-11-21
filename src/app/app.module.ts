7c61c6158c2ba3b44b5cb4819f176f58ab82a9a6
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2022-11-21 17:49:34 +0300

    feat: packages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CategoryComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
