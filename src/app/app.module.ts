1f13846375e2329a4208a6d66fb6318af9c9894a
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2022-11-26 13:24:50 +0300

    feat: new module added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { CategoryComponent } from './components/category/category.component';
 import { ProductComponent } from './components/product/product.component';
+import { ProductFilterPipe } from './Pipes/productPipe/product-filter.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     CategoryComponent,
-    ProductComponent
+    ProductComponent,
+    ProductFilterPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
