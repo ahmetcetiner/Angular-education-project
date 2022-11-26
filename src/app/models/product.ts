b0f6956e44dae9bc6886c45651debf0a0fa1021c
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2022-11-26 12:34:49 +0300

    feat: models added.

---
+++ src/app/models/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+  id?: number;
+  catagoryID?: number;
+  productImg?: string;
+  productName?: string;
+  property_1?: string;
+  property_2?: string;
+  property_3?: string;
+  property_4?: string;
+  property_5?: string;
+  property_6?: string;
+  productPrice?: number;
+}
