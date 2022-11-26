f5438090d09831b883e83f7fd0be4ee6140b5124
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2022-11-26 14:30:57 +0300

    feat: alertifyjs package and alertify service add

---
+++ src/app/services/alertify.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+declare let alertify:any;
+
+@Injectable()
+export class AlertifyService {
+
+  constructor() { }
+
+  success(message:string){
+    alertify.success(message)
+  }
+}
