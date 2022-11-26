f845a1028c9466958b40e6703895c7306a37dd9f
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2022-11-26 13:26:11 +0300

    feat: product filter added and revised

---
+++ src/app/Pipes/productPipe/product-filter.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from 'src/app/models/product';
+
+@Pipe({
+  name: 'productFilter'
+})
+export class ProductFilterPipe implements PipeTransform {
+
+  transform(value: Product[], filterText: string): Product[] {
+    filterText = filterText?filterText.toLocaleLowerCase():null!
+    return filterText?value.filter((p:Product)=>p.productName?.toLocaleLowerCase().indexOf(filterText)!==-1):value;
+  }
+
+}
