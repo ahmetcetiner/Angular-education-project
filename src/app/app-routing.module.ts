191b4ee9c59f1f65f932ee1a1cd565fef6b245b4
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2022-11-29 14:41:50 +0300

    refactor: revised routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product',
+    component: ProductComponent,
+    data: { title: 'Products' },
+  },
+  {
+    path: 'product/category/:categoryID',
+    component: ProductComponent,
+    data: { title: 'Products' },
+  },
+  {
+    path: '',
+    redirectTo: 'product',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
