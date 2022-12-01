b652ab310a8c6394371b4f0f444ed09ee1d0f0f9
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2022-12-01 16:12:15 +0300

    feat: navbar revised

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DarkModeService } from 'angular-dark-mode';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private darkModeService: DarkModeService) { }
+  darkMode$: Observable<boolean> = this.darkModeService.darkMode$;
 
   ngOnInit(): void {
+  }
+
+  onToggle(): void {
+
+    this.darkModeService.toggle();
   }
 
 }
